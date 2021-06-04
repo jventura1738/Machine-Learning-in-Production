862ef4905fcb76f0ce6b255b36b36744cd9eedd6
j.ventura1738 <justinv9001@aol.com>
2021-06-04 17:22:15 -0400

    still need to add capability to write to file from direct script

--- scripts/direct.py
+++ scripts/direct.py
@@ -2,11 +2,14 @@
 
 # Libraries:
 import os
-# import numpy as np
-import pandas as pd
+import sys
 import stscraper as scraper
 
+# IMPORTANT: CONSTANTS
 TOKENS = os.getenv('GITHUB_API_TOKENS', default=None)
+FIELDS = ['category', 'gh_id', 'name', 'full_name', 'owner',
+          'desc', 'created', 'last_update', 'size', 'forks',
+          'stars', 'language']
 
 """
 Short script for direct access to GitHub repos via
@@ -18,45 +21,55 @@ the GitHub API.
 # noinspection PyUnresolvedReferences
 def _main():
     # This is the API object which will be interacted with.
-    # NOTE: this is for APIv3 currently.
+    # NOTE: this is for GitHubAPIv3 currently.
     gh_api = scraper.GitHubAPI(TOKENS)
 
-    # NOTE: format as <username/repository_name>
-    targ_slug = input('Enter target repo name: ')
-    choice = str(input('Write to file? y/n'))
-    assert(choice == 'y' or choice == 'n'), 'y/n'
+    data_list = list()
 
-    # Scrape and store here:
-    targ_info = gh_api.repo_info(targ_slug)
+    # Iterate over the commandline arguments:
+    for repo_name in sys.argv:
 
-    if choice == 'y':
-        # Create/write to the file.  Will overwrite!
-        mid = targ_slug.index('/')
-        fp = open(f'{targ_slug[mid + 1:]}.txt', 'w')
+        # NOTE: format as <username/repository_name>
+        targ_slug = str(repo_name)
+        # choice = str(input('Write to file? y/n'))
+        # assert(choice == 'y' or choice == 'n'), 'y/n'
 
-        # noinspection PyUnresolvedReferences
-        for k, v in targ_info.items():
-            fp.write(f'{k}: {v}\n')
-        fp.close()
+        # Scrape and store here:
+        targ_info = gh_api.repo_info(targ_slug)
 
-    # This is where useful data is stored:
-    df_data = dict()
+        # if choice == 'y':
+        #     # Create/write to the file.  Will overwrite!
+        #     mid = targ_slug.index('/')
+        #     fp = open(f'{targ_slug[mid + 1:]}.txt', 'w')
+        #
+        #     # noinspection PyUnresolvedReferences
+        #     for k, v in targ_info.items():
+        #         fp.write(f'{k}: {v}\n')
+        #     fp.close()
 
-    # TODO: figure out the commented sections below.
-    df_data['category'] = 'N/A'
-    df_data['gh_id'] = targ_info['id']
-    df_data['name'] = targ_info['name']
-    df_data['full_name'] = targ_info['full_name']
-    df_data['owner'] = targ_info['name']
-    df_data['desc'] = targ_info['description']
-    df_data['created'] = targ_info['created_at']
-    # df_data['last_commit'] = targ_info['updated_at']
-    df_data['last_update'] = targ_info['updated_at']
-    df_data['size'] = targ_info['size']
-    df_data['forks'] = targ_info['forks']
-    df_data['stars'] = targ_info['stargazers_count']
-    # df_data['contributors'] = targ_info['name']
-    df_data['language'] = targ_info['language']
+        # This is where useful data is stored:
+        df_data = dict()
+
+        # TODO: figure out the commented sections below.
+        df_data['category'] = 'N/A'
+        df_data['gh_id'] = targ_info['id']
+        df_data['name'] = targ_info['name']
+        df_data['full_name'] = targ_info['full_name']
+        df_data['owner'] = targ_info['name']
+        df_data['desc'] = targ_info['description']
+        df_data['created'] = targ_info['created_at']
+        # df_data['last_commit'] = targ_info['updated_at']
+        df_data['last_update'] = targ_info['updated_at']
+        df_data['size'] = targ_info['size']
+        df_data['forks'] = targ_info['forks']
+        df_data['stars'] = targ_info['stargazers_count']
+        # df_data['contributors'] = targ_info['name']
+        df_data['language'] = targ_info['language']
+
+        data_list.append(df_data)
+
+    print(data_list)
+
 
 # Ensure directly running the script; not a module.
 if __name__ == '__main__':
