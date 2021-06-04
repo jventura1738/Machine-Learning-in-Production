53bc689d7b4d202bcfbbb648ea797623f352bebb
j.ventura1738 <justinv9001@aol.com>
2021-06-04 10:56:48 -0400

    update

---
+++ scripts/phase1.py
@@ -0,0 +1,31 @@
+# Justin Ventura | Carnegie Mellon University
+
+# Libraries:
+import numpy as np
+
+"""
+DESCRIPTION:
+This script will be used to scrape (primarily) github
+for software with the criteria we need it to fit.
+
+THE CHALLENGE: we are NOT looking for frameworks, APIs,
+school projects, student work, demos for research papers
+or thesis', notebook experiments, nor hackathon projects.
+
+WHAT WE WANT: open source commercial software with some
+sort of ML/AI implementation.  Simple filters will not
+yield desired results, so we are still trying to figure
+out an effective way to scrape.  Brute forcing is not
+an option since it would require human validation which
+is astronomically slow, and painful.
+
+IDEAS: ???
+"""
+
+# TODO: perform a basic search through tags using kws.
+def _main():
+    pass
+
+
+if __name__ == '__main__':
+    _main()
