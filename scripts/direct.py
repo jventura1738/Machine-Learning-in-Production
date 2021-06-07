a0ed2089597dc4fa17e334a5036e5173713f3135
j.ventura1738 <justinv9001@aol.com>
2021-06-07 11:04:32 -0400

    removed txts and fixed typo in script

--- scripts/direct.py
+++ scripts/direct.py
@@ -27,7 +27,7 @@ def _main():
     data_list = list()
 
     # Iterate over the commandline arguments:
-    for repo_name in sys.argv:
+    for repo_name in sys.argv[1:]:
 
         # NOTE: format as <username/repository_name>
         targ_slug = str(repo_name)
@@ -55,7 +55,7 @@ def _main():
         df_data['gh_id'] = targ_info['id']
         df_data['name'] = targ_info['name']
         df_data['full_name'] = targ_info['full_name']
-        df_data['owner'] = targ_info['name']
+        # df_data['owner'] = targ_info['owner']
         df_data['desc'] = targ_info['description']
         df_data['created'] = targ_info['created_at']
         # df_data['last_commit'] = targ_info['updated_at']
@@ -68,7 +68,8 @@ def _main():
 
         data_list.append(df_data)
 
-    print(data_list)
+    for d in data_list:
+        print(d, end='\n\n')
 
 
 # Ensure directly running the script; not a module.
